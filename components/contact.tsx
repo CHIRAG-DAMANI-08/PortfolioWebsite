d783afd84c82cd16340fa4184596b39759a3d898
chirag Damani <chiragdamani2003@gmail.com>
2024-02-14 16:07:58 +0530

    Final commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
       <p className="text-gray-700 mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+          22BTRSN010@jainuniversity.ac.in
         </a>{" "}
         , (+91) 9606507272 or through this form.
       </p>
